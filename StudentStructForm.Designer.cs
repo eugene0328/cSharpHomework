8592270219feb750cea69049fa76981d715d2784
eugene0328 <eugene0328@gmail.com>
2022-06-28 15:21:15 +0800

    完成ssform功能實作

--- StudentStructForm.Designer.cs
+++ StudentStructForm.Designer.cs
@@ -40,20 +40,19 @@ namespace MyHomework
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox1.Location = new System.Drawing.Point(159, 84);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(119, 36);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 36);
+            this.textBox1.Size = new System.Drawing.Size(147, 30);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "--";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -61,10 +60,9 @@ namespace MyHomework
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox2.Location = new System.Drawing.Point(159, 142);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(119, 83);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(195, 36);
+            this.textBox2.Size = new System.Drawing.Size(147, 30);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = "0";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -72,10 +70,9 @@ namespace MyHomework
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox3.Location = new System.Drawing.Point(159, 202);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Location = new System.Drawing.Point(119, 131);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(195, 36);
+            this.textBox3.Size = new System.Drawing.Size(147, 30);
             this.textBox3.TabIndex = 2;
             this.textBox3.Text = "0";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -83,10 +80,9 @@ namespace MyHomework
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox4.Location = new System.Drawing.Point(159, 262);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Location = new System.Drawing.Point(119, 179);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(195, 36);
+            this.textBox4.Size = new System.Drawing.Size(147, 30);
             this.textBox4.TabIndex = 3;
             this.textBox4.Text = "0";
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -95,12 +91,11 @@ namespace MyHomework
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(59, 91);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label1.Location = new System.Drawing.Point(44, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.Size = new System.Drawing.Size(55, 19);
             this.label1.TabIndex = 4;
             this.label1.Text = "姓名:";
             // 
@@ -108,12 +103,11 @@ namespace MyHomework
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(59, 150);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label2.Location = new System.Drawing.Point(44, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.Size = new System.Drawing.Size(55, 19);
             this.label2.TabIndex = 5;
             this.label2.Text = "國文:";
             // 
@@ -121,12 +115,11 @@ namespace MyHomework
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(59, 209);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label3.Location = new System.Drawing.Point(44, 136);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.Size = new System.Drawing.Size(55, 19);
             this.label3.TabIndex = 6;
             this.label3.Text = "英文:";
             // 
@@ -134,71 +127,84 @@ namespace MyHomework
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(59, 269);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label4.Location = new System.Drawing.Point(44, 184);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 24);
+            this.label4.Size = new System.Drawing.Size(55, 19);
             this.label4.TabIndex = 7;
             this.label4.Text = "數學:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(64, 322);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(48, 227);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 48);
+            this.button1.Size = new System.Drawing.Size(106, 38);
             this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
+            this.button1.Text = "儲存";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(213, 322);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(160, 227);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(141, 48);
+            this.button2.Size = new System.Drawing.Size(106, 38);
             this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
+            this.button2.Text = "顯示儲存內容";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(434, 286);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 38);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "最高/最低分科目";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Lavender;
-            this.groupBox1.Controls.Add(this.splitContainer1);
-            this.groupBox1.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.groupBox1.ForeColor = System.Drawing.Color.Indigo;
-            this.groupBox1.Location = new System.Drawing.Point(425, 84);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(319, 36);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(295, 286);
-            this.groupBox1.TabIndex = 10;
+            this.groupBox1.Size = new System.Drawing.Size(221, 229);
+            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "成績";
             // 
-            // splitContainer1
+            // label5
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(4, 27);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            this.splitContainer1.Size = new System.Drawing.Size(287, 255);
-            this.splitContainer1.SplitterDistance = 148;
-            this.splitContainer1.SplitterWidth = 5;
-            this.splitContainer1.TabIndex = 0;
+            this.label5.BackColor = System.Drawing.Color.AliceBlue;
+            this.label5.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(6, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(209, 109);
+            this.label5.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.AliceBlue;
+            this.label6.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(6, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 83);
+            this.label6.TabIndex = 1;
             // 
             // StudentStructForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(784, 445);
+            this.ClientSize = new System.Drawing.Size(588, 356);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -209,12 +215,9 @@ namespace MyHomework
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "StudentStructForm";
             this.Text = "StudentStructForm";
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,7 +235,9 @@ namespace MyHomework
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
--- StudentStructForm.cs
+++ StudentStructForm.cs
@@ -17,18 +17,95 @@ namespace MyHomework
             InitializeComponent();
         }
 
-        string name, chn, en, math, result;
+        string name, chn, en, math, result1, result2, highest, lowest;
+        int[] score = new int [3] {1,2,3};
 
         private void button1_Click(object sender, EventArgs e)
         {
             name = textBox1.Text;
             chn = textBox2.Text;
-
+            en = textBox3.Text;
+            math = textBox4.Text;
+            result1 = "姓名: "+ name +"\n國文: " + chn + "\n英文: " + en + "\n數學: " + math;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if ("".Equals(Check()))
+            {
+                Lab5Show();
+            }
+            else
+            {
+                MessageBox.Show(Check());
+            }
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ("".Equals(Check()))
+                {
+                    score[0] = int.Parse(chn);
+                    score[1] = int.Parse(en);
+                    score[2] = int.Parse(math);
+                    Lab6Show();
+                    label6.Text = result2;
+                }
+                else
+                {
+                    MessageBox.Show(Check());
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("輸入格式錯誤或沒輸入成績");
+                Console.WriteLine(ex);
+            }                    
+        }
+
+        private string Check()
+        {
+            string chkmsg = "";
+            if ("".Equals(result1) || result1 == null) {
+                chkmsg = "請先儲存";
+            }
+            return chkmsg;
+        }
+
+        private void Lab5Show()
+        {
+            label5.Text = result1;
+        }
+
+        private void Lab6Show()
+        {
+            int max = score.Max();
+            int min = score.Min();
+            if (max == score[0])
+            {
+                highest = "國文" + chn + "分";
+            }
+            else if (max == score[1]) {
+                highest = "英文" + en + "分";
+            }
+            else if (max == score[2])
+            {
+                highest = "數學" + math + "分";
+            }
+            if (min == score[0])
+            {
+                lowest = "國文" + chn + "分";
+            }
+            else if (min == score[1])
+            {
+                lowest = "英文" + en + "分";
+            }
+            else if (min == score[2])
+            {
+                lowest = "數學" + math + "分";
+            }
+            result2 = "最高科目成績為: " + highest + "\n最低科目成績為: " + lowest;
         }
     }
 }
