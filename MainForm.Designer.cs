d317559e490b469f07d7e214c512b44e1d9c807b
eugene0328 <eugene0328@gmail.com>
2022-07-12 14:16:59 +0800

    大小調整

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,6 +31,7 @@ namespace MyHomework
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button16 = new System.Windows.Forms.Button();
             this.button15 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
@@ -48,7 +49,6 @@ namespace MyHomework
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -60,12 +60,23 @@ namespace MyHomework
             this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(-1, 0);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(2115, 134);
+            this.groupBox1.Size = new System.Drawing.Size(1586, 107);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "C#作業";
             // 
             // button16
             // 
@@ -73,10 +84,9 @@ namespace MyHomework
             this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button16.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button16.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button16.Location = new System.Drawing.Point(143, 849);
-            this.button16.Margin = new System.Windows.Forms.Padding(4);
+            this.button16.Location = new System.Drawing.Point(107, 679);
             this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(413, 38);
+            this.button16.Size = new System.Drawing.Size(310, 30);
             this.button16.TabIndex = 15;
             this.button16.Text = "Alarm";
             this.button16.UseVisualStyleBackColor = false;
@@ -87,10 +97,9 @@ namespace MyHomework
             this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button15.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button15.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button15.Location = new System.Drawing.Point(143, 796);
-            this.button15.Margin = new System.Windows.Forms.Padding(4);
+            this.button15.Location = new System.Drawing.Point(107, 637);
             this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(413, 38);
+            this.button15.Size = new System.Drawing.Size(310, 30);
             this.button15.TabIndex = 14;
             this.button15.Text = "GuessNum";
             this.button15.UseVisualStyleBackColor = false;
@@ -102,10 +111,9 @@ namespace MyHomework
             this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button14.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button14.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button14.Location = new System.Drawing.Point(143, 744);
-            this.button14.Margin = new System.Windows.Forms.Padding(4);
+            this.button14.Location = new System.Drawing.Point(107, 595);
             this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(413, 38);
+            this.button14.Size = new System.Drawing.Size(310, 30);
             this.button14.TabIndex = 13;
             this.button14.Text = "Picture Viewer";
             this.button14.UseVisualStyleBackColor = false;
@@ -116,10 +124,9 @@ namespace MyHomework
             this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button13.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button13.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button13.Location = new System.Drawing.Point(143, 691);
-            this.button13.Margin = new System.Windows.Forms.Padding(4);
+            this.button13.Location = new System.Drawing.Point(107, 553);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(413, 38);
+            this.button13.Size = new System.Drawing.Size(310, 30);
             this.button13.TabIndex = 12;
             this.button13.Text = "Painter";
             this.button13.UseVisualStyleBackColor = false;
@@ -130,10 +137,9 @@ namespace MyHomework
             this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button12.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button12.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button12.Location = new System.Drawing.Point(143, 639);
-            this.button12.Margin = new System.Windows.Forms.Padding(4);
+            this.button12.Location = new System.Drawing.Point(107, 511);
             this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(413, 38);
+            this.button12.Size = new System.Drawing.Size(310, 30);
             this.button12.TabIndex = 11;
             this.button12.Text = "Notepad";
             this.button12.UseVisualStyleBackColor = false;
@@ -144,10 +150,9 @@ namespace MyHomework
             this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button11.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button11.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button11.Location = new System.Drawing.Point(143, 586);
-            this.button11.Margin = new System.Windows.Forms.Padding(4);
+            this.button11.Location = new System.Drawing.Point(107, 469);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(413, 38);
+            this.button11.Size = new System.Drawing.Size(310, 30);
             this.button11.TabIndex = 10;
             this.button11.Text = "Screen Saver";
             this.button11.UseVisualStyleBackColor = false;
@@ -158,10 +163,9 @@ namespace MyHomework
             this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button10.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button10.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button10.Location = new System.Drawing.Point(143, 534);
-            this.button10.Margin = new System.Windows.Forms.Padding(4);
+            this.button10.Location = new System.Drawing.Point(107, 427);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(413, 38);
+            this.button10.Size = new System.Drawing.Size(310, 30);
             this.button10.TabIndex = 9;
             this.button10.Text = "Tic-tac-toe";
             this.button10.UseVisualStyleBackColor = false;
@@ -172,10 +176,9 @@ namespace MyHomework
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button9.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button9.Location = new System.Drawing.Point(143, 481);
-            this.button9.Margin = new System.Windows.Forms.Padding(4);
+            this.button9.Location = new System.Drawing.Point(107, 385);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(413, 38);
+            this.button9.Size = new System.Drawing.Size(310, 30);
             this.button9.TabIndex = 8;
             this.button9.Text = "ForDoWhile";
             this.button9.UseVisualStyleBackColor = false;
@@ -186,10 +189,9 @@ namespace MyHomework
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button8.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button8.Location = new System.Drawing.Point(143, 429);
-            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Location = new System.Drawing.Point(107, 343);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(413, 38);
+            this.button8.Size = new System.Drawing.Size(310, 30);
             this.button8.TabIndex = 7;
             this.button8.Text = "MyClac";
             this.button8.UseVisualStyleBackColor = false;
@@ -201,10 +203,9 @@ namespace MyHomework
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button7.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button7.Location = new System.Drawing.Point(143, 376);
-            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Location = new System.Drawing.Point(107, 301);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(413, 38);
+            this.button7.Size = new System.Drawing.Size(310, 30);
             this.button7.TabIndex = 6;
             this.button7.Text = "Method";
             this.button7.UseVisualStyleBackColor = false;
@@ -215,10 +216,9 @@ namespace MyHomework
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button6.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button6.Location = new System.Drawing.Point(143, 324);
-            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Location = new System.Drawing.Point(107, 259);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(413, 38);
+            this.button6.Size = new System.Drawing.Size(310, 30);
             this.button6.TabIndex = 5;
             this.button6.Text = "StudentGradeList";
             this.button6.UseVisualStyleBackColor = false;
@@ -230,10 +230,9 @@ namespace MyHomework
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button5.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button5.Location = new System.Drawing.Point(143, 271);
-            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Location = new System.Drawing.Point(107, 217);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(413, 38);
+            this.button5.Size = new System.Drawing.Size(310, 30);
             this.button5.TabIndex = 4;
             this.button5.Text = "StudentGrade";
             this.button5.UseVisualStyleBackColor = false;
@@ -245,10 +244,9 @@ namespace MyHomework
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button4.Location = new System.Drawing.Point(143, 219);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(107, 175);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(413, 38);
+            this.button4.Size = new System.Drawing.Size(310, 30);
             this.button4.TabIndex = 3;
             this.button4.Text = "StudentStructForm";
             this.button4.UseVisualStyleBackColor = false;
@@ -260,10 +258,9 @@ namespace MyHomework
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button3.Location = new System.Drawing.Point(143, 166);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(107, 133);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(413, 38);
+            this.button3.Size = new System.Drawing.Size(310, 30);
             this.button3.TabIndex = 2;
             this.button3.Text = "POS";
             this.button3.UseVisualStyleBackColor = false;
@@ -275,10 +272,9 @@ namespace MyHomework
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(143, 114);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(107, 91);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(413, 38);
+            this.button2.Size = new System.Drawing.Size(310, 30);
             this.button2.TabIndex = 1;
             this.button2.Text = "Loan";
             this.button2.UseVisualStyleBackColor = false;
@@ -290,10 +286,9 @@ namespace MyHomework
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(143, 61);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(107, 49);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(413, 38);
+            this.button1.Size = new System.Drawing.Size(310, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = false;
@@ -301,8 +296,7 @@ namespace MyHomework
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Location = new System.Drawing.Point(-1, 132);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Location = new System.Drawing.Point(-1, 106);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -329,31 +323,17 @@ namespace MyHomework
             // 
             this.splitContainer1.Panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("splitContainer1.Panel2.BackgroundImage")));
             this.splitContainer1.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.splitContainer1.Size = new System.Drawing.Size(2115, 945);
-            this.splitContainer1.SplitterDistance = 702;
-            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.Size = new System.Drawing.Size(1586, 756);
+            this.splitContainer1.SplitterDistance = 526;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.label1.Location = new System.Drawing.Point(34, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "C#作業";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.ClientSize = new System.Drawing.Size(1584, 861);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "Main";
             this.groupBox1.ResumeLayout(false);
